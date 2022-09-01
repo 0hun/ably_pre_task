84f40e1f8489b6cc6af273f79e1d41c0547a06e3
younghun <younghun@paycurx.co.kr>
2022-09-01 15:41:01 +0900

    feat (feature) : init project
    feat (feature) : init project
    - 프로젝트 생성 및 초기화

---
+++ src/main/java/com/ably/pre_task/PreTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ably.pre_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ably/pre_task/PreTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ably.pre_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
